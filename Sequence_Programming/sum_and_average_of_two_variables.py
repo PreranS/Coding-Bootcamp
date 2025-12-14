e127dcd1771a7f387a97f3729546631b9aa509fe
Preran <prean2003@gmail.com>
2025-12-15 00:23:40 +0530

    Add solution for Coding Challenge 1

---
+++ Sequence_Programming/sum_and_average_of_two_variables.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+sum_result = a + b
+average = sum_result / 2
+
+print("Sum:", sum_result)
+print("Average:", average)
